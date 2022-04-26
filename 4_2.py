e7dd0d4db045425ebde75d1f2c69532892c95a5d
Andres C. Perez <Titas23@Andress-MacBook-Air.local>
2022-04-26 15:17:32 -0700

    4_2 done

---
+++ 4_2.py
@@ -0,0 +1,42 @@
+# # def main (): 
+# #     first_part()
+# #     print(str(4) + ": from function main")
+
+# # def first_part (): 
+# #     print(str(1) + ": from function first_part")
+# #     second_part()
+# #     print(str(3) + ": from function first_part")
+
+# # def second_part():
+# #     print(str(2) + ": from function second_part")
+
+# # main()
+
+# # list1 = ["a", "e", "i", "o", "u"]
+
+
+# # def f(y):
+# #     return y.upper()
+
+# # # for x in list1:
+# # #     print(x.upper())
+# # #     list2.append(f(x))
+ 
+# # # print(list2)
+
+
+# # list2 = [f(x) for x in list1]
+# # print(list2)
+
+# print("List: ", [x.upper() for x in ["a", "e", "i", "o", "u"]])
+
+# def total(w, x, y=10, z =20):
+#     return ( w ** x) + y + z
+
+# print(total(2, 3))
+# print(total(2, 3, 4))
+# print(total(2, 3, 3, 4))
+# print(total(2, 3))
+
+
+
